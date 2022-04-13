89a92f13af34982d57e22ff78866ebc631198d62
achommatur <achommatur@outlook.com>
2022-04-13 13:32:13 +0200

    manipulated the DOM to make the game interactive

--- rockPaperScissors/pages/script.js
+++ rockPaperScissors/pages/script.js
@@ -1,101 +1,68 @@
+const lesChoix = document.querySelectorAll('.choix');
+const leDeployer = Array.from(lesChoix);
+const leJouer = document.getElementById('player')
+const lOrdinateur = document.getElementById('computer');
+const comp = document.querySelector('.comp');
+const jouse = document.querySelector('.jouse');
+const choixDOrdinateur = function () 
+{
+   return leDeployer[Math.floor(Math.random() * leDeployer.length)];
+};
 
-// const winDecider = true;
-let playerWinCount = 0;
-let computerWinCount = 0;
-// function computerPlay(choices) 
-// {
-//     return choices[Math.floor(Math.random()*choices.length)]
+function playRound (element)
+{
+  leJouer.innerHTML = element.innerHTML;
+  leJouer.classList.add('iconed');
+  leJouer.classList.remove('choix');
+  lOrdinateur.innerHTML = choixDOrdinateur().innerHTML;
+  lOrdinateur.classList.add('iconed');
+  lOrdinateur.classList.remove('choix');
+  const papier = leDeployer[0].innerHTML;
+  const pierre = leDeployer[1].innerHTML;
+  const ciseaux = leDeployer[2].innerHTML;
+  switch (true)
+  {
+   case lOrdinateur.innerHTML===leJouer.innerHTML:
+   comp.innerHTML = "<div class='outcome'>=</div>";
+   jouse.innerHTML = "<div class='outcome'>=</div>";
+   break;
+   case lOrdinateur.innerHTML=== ciseaux && leJouer.innerHTML === pierre:
+      jouse.innerHTML =
+       '<div class="win"><img src="../images/black-24dp (2)/2x/outline_done_black_24dp.png"></div>';
+      comp.innerHTML =
+       '<div class="loss"><img src="../images/black-24dp/2x/outline_dangerous_black_24dp.png"></div>';
+       break;
+   case lOrdinateur.innerHTML=== ciseaux && leJouer.innerHTML === papier:
+      comp.innerHTML =
+       '<div class="win"><img src="../images/black-24dp (2)/2x/outline_done_black_24dp.png"></div>';
+       jouse.innerHTML =
+       '<div class="loss"><img src="../images/black-24dp/2x/outline_dangerous_black_24dp.png"></div>';
+       break;
+   case lOrdinateur.innerHTML=== pierre && leJouer.innerHTML === ciseaux:
+      comp.innerHTML =
+       '<div class="win"><img src="../images/black-24dp (2)/2x/outline_done_black_24dp.png"></div>';
+      jouse.innerHTML =
+       '<div class="loss"><img src="../images/black-24dp/2x/outline_dangerous_black_24dp.png"></div>';
+       break;
+   case lOrdinateur.innerHTML=== pierre && leJouer.innerHTML === papier:
+      jouse.innerHTML =
+       '<div class="win"><img src="../images/black-24dp (2)/2x/outline_done_black_24dp.png"></div>';
+      comp.innerHTML =
+       '<div class="loss"><img src="../images/black-24dp/2x/outline_dangerous_black_24dp.png"></div>';
+       break;
+   case lOrdinateur.innerHTML=== papier && leJouer.innerHTML === ciseaux:
+      jouse.innerHTML =
+       '<div class="win"><img src="../images/black-24dp (2)/2x/outline_done_black_24dp.png"></div>';
+      comp.innerHTML =
+       '<div class="loss"><img src="../images/black-24dp/2x/outline_dangerous_black_24dp.png"></div>';
+       break;
+   case lOrdinateur.innerHTML=== papier && leJouer.innerHTML === pierre:
+      comp.innerHTML =
+       '<div class="win"><img src="../images/black-24dp (2)/2x/outline_done_black_24dp.png"></div>';
+      jouse.innerHTML =
+       '<div class="loss"><img src="../images/black-24dp/2x/outline_dangerous_black_24dp.png"></div>';
+       break;
+  }
+  };
 
-// }
-// computerPlay(['rock','paper','scissors']);
-
-
-// function playRound(playerSelection, computerSelection)
-//  {
-     
-// switch(true)
-// {
-//     case playerSelection===computerSelection:
-        
-//         return ('Computer\'s selection: '+computerSelection+
-//         "\nIt's a draw"+
-//         '\n ------------------------------'+
-//             '\nYour Wins: ' +playerWinCount+' Computer\'s wins: '+computerWinCount+
-//             '\n ------------------------------'); ;
-//         break;
-//     case playerSelection === 'rock' && computerSelection === 'paper':
-//         computerWinCount++;
-//         return ('Computer\'s selection: '+computerSelection+
-//         " \nYou lose, paper beats rock"+
-//         '\n ------------------------------'+
-//             '\nYour Wins: ' +playerWinCount+' Computer\'s wins: '+computerWinCount+
-//             '\n ------------------------------'); ;
-//         break;
-//         case playerSelection === 'paper' && computerSelection === 'rock':
-//             playerWinCount++;
-//             return ('Computer\'s selection: '+computerSelection+
-//             "\n You win, paper beats rock"+
-//             '\n ------------------------------'+
-//             '\nYour Wins: ' +playerWinCount+' Computer\'s wins: '+computerWinCount+
-//             '\n ------------------------------'); ;
-//         break;
-//         case playerSelection === 'scissors' && computerSelection === 'paper':
-//             playerWinCount++;
-//             return ('Computer\'s selection: '+computerSelection+
-//             " \nYou win, scissors beats paper"+
-//             '\n ------------------------------'+
-//             '\nYour Wins: ' +playerWinCount+' Computer\'s wins: '+computerWinCount+
-//             '\n ------------------------------'); ;
-//         break;
-//         case playerSelection === 'scissors' && computerSelection === 'rock':
-//             computerWinCount++;
-//             return ('Computer\'s selection: '+computerSelection+
-//             " \nYou lose, rock beats scissors"+
-//             '\n ------------------------------'+
-//             '\nYour Wins: ' +playerWinCount+' Computer\'s wins: '+computerWinCount+
-//             '\n ------------------------------'); ;
-//         break;
-//         case playerSelection === 'paper' && computerSelection === 'scissors':
-//             computerWinCount++;
-//             return ('Computer\'s selection: '+computerSelection+
-//             " \nYou lose, scissors beats paper"+
-//             '\n ------------------------------'+
-//             '\nYour Wins: ' +playerWinCount+' Computer\'s wins: '+computerWinCount+
-//             '\n ------------------------------'); ;
-//         break;
-//         case playerSelection === 'rock' && computerSelection === 'scissors':
-//             playerWinCount++;
-//             return('Computer\'s selection: '+ computerSelection+ 
-//             " \nYou win, rock beats scissors" + 
-//             '\n ------------------------------'+
-//             '\nYour Wins: ' +playerWinCount+' Computer\'s wins: '+computerWinCount+
-//             '\n ------------------------------'); 
-        
-// }
-//   }
-  
-//   const playerSelection = prompt('Rock, Paper, Scissors!').toLowerCase();
-//   const computerSelection = computerPlay(['rock','paper','scissors']);
-//   console.log(playRound(playerSelection, computerSelection))
-//   function game ()
-//   {
-//     for (let i = 1; i < 5; i++)
-//     {       
-//         const playerSelection = prompt('Rock, Paper, Scissors!').toLowerCase(); 
-//         const computerSelection = computerPlay(['rock','paper','scissors']);
-//          console.log(playRound(playerSelection, computerSelection));
-        
-//     }
-//   }
-//   game();
-//   if (playerWinCount>computerWinCount)
-//   {
-//       console.log('Hurray!! You win :)')
-//   }
-//   else if (playerWinCount===computerWinCount)
-//   {
-//       console.log('it\'s a draw :3')
-//   }
-// else{
-//     console.log('Ohh No!!You lose :(')
-// }
+leDeployer.forEach(item => item.addEventListener('click', ()=> playRound(item)))
